9a4999fb7a53400f52f86f723507fd9b36ff6bd2
vtew <195099622+vtew@users.noreply.github.com>
2026-03-22 19:57:52 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -7,6 +7,6 @@ curl -fsSL https://github.com/SagerNet/sing-box/releases/download/v1.11.4/sing-b
   | tar -xz --strip-components=1
 curl -fsSL -o cloudflared https://github.com/cloudflare/cloudflared/releases/download/2026.3.0/cloudflared-linux-$arch
 chmod u+x cloudflared
-curl -fsSL -o config.json https://raw.githubusercontent.com/vtew/docker-vless/refs/heads/main/config-template.json
+curl -fsSL -o config.json https://raw.githubusercontent.com/vtew/docker-vless/refs/heads/main/config.json.template
 curl -fsSL https://raw.githubusercontent.com/vtew/docker-vless/refs/heads/main/docker-entrypoint.sh \
   | PATH=.:$PATH bash
