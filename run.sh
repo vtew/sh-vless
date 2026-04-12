a114c6160f89494e8cf0848bb8bc9f9344e95d41
vtew <195099622+vtew@users.noreply.github.com>
2026-04-12 12:35:42 +0800

    Refactor run.sh for improved readability and syntax

--- run.sh
+++ run.sh
@@ -1,12 +1,17 @@
-#!/usr/bin/env bash
-set -euo pipefail
-[ -f .env ] && { set -a; . .env; set +a; }
+#!/bin/bash
+
+set -e
+
+[[ -f .env ]] && { set -a; . .env; set +a; }
+
 declare -A arch=( ["x86_64"]="amd64" ["aarch64"]="arm64" )
 arch=${arch[$(uname -m)]:?Unsupported arch: $(uname -m)}
-curl -fsSL https://github.com/SagerNet/sing-box/releases/download/v1.11.4/sing-box-1.11.4-linux-$arch.tar.gz \
-  | tar -xz --strip-components=1
-curl -fsSL -o cloudflared https://github.com/cloudflare/cloudflared/releases/download/2026.3.0/cloudflared-linux-$arch
+
+curl -sL https://github.com/SagerNet/sing-box/releases/download/v1.11.4/sing-box-1.11.4-linux-$arch.tar.gz |
+  tar -xz --strip-components=1
+
+curl -sL -o cloudflared https://github.com/cloudflare/cloudflared/releases/download/2026.3.0/cloudflared-linux-$arch
 chmod u+x cloudflared
-curl -fsSL -o config.json https://raw.githubusercontent.com/vtew/docker-vless/refs/heads/main/config.json.template
-curl -fsSL https://raw.githubusercontent.com/vtew/docker-vless/refs/heads/main/docker-entrypoint.sh \
-  | PATH=.:$PATH bash
+
+curl -s https://raw.githubusercontent.com/vtew/docker-vless/refs/heads/main/docker-entrypoint.sh |
+  PATH=.:$PATH exec bash
